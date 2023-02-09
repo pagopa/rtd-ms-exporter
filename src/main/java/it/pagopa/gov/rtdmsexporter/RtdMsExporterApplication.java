0f2167d0140b8d166946658789e1b9c656baa6f5
andrea.petreti <andrea.petreti@pagopa.it>
2023-02-09 11:04:13 +0100

    initial commit

---
+++ src/main/java/it/pagopa/gov/rtdmsexporter/RtdMsExporterApplication.java
@@ -0,0 +1,13 @@
+package it.pagopa.gov.rtdmsexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtdMsExporterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RtdMsExporterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/it/pagopa/gov/rtdmsexporter/RtdMsExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pagopa.gov.rtdmsexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtdMsExporterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
