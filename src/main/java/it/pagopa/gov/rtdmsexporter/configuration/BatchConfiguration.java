9c9c8d790e66eb4b5fb6fc94ee44386ee8c16ed6
andrea.petreti <andrea.petreti@pagopa.it>
2023-02-10 00:20:24 +0100

    h2 batch configuration

---
+++ src/main/java/it/pagopa/gov/rtdmsexporter/configuration/BatchConfiguration.java
@@ -0,0 +1,26 @@
+package it.pagopa.gov.rtdmsexporter.configuration;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableBatchProcessing
+@EnableScheduling
+public class BatchConfiguration {
+
+  @Bean
+  protected DataSource dataSource() {
+    EmbeddedDatabaseBuilder embeddedDatabaseBuilder = new EmbeddedDatabaseBuilder();
+    return embeddedDatabaseBuilder
+            .setType(EmbeddedDatabaseType.H2)
+            .build();
+  }
+
+
+}
