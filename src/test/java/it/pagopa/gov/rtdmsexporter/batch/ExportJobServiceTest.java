512a0467d78fa3b777e2a5e615ddc9166b681d67
andrea.petreti <andrea.petreti@pagopa.it>
2023-02-17 12:52:07 +0100

    add test to schedule job

---
+++ src/test/java/it/pagopa/gov/rtdmsexporter/batch/ExportJobServiceTest.java
@@ -0,0 +1,47 @@
+package it.pagopa.gov.rtdmsexporter.batch;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.test.MetaDataInstanceFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class ExportJobServiceTest {
+
+  private JobLauncher jobLauncher;
+  private ExportJobService exportJobService;
+
+  private static final String TEST_ACQUIRER_FILE = "file.csv";
+  private static final int TEST_CHUNK_SIZE = 20;
+
+  @BeforeEach
+  void setup() {
+    jobLauncher = mock(JobLauncher.class);
+    exportJobService = new ExportJobService(
+            jobLauncher,
+            mock(Job.class),
+            TEST_ACQUIRER_FILE,
+            TEST_CHUNK_SIZE
+    );
+  }
+
+  @Test
+  void shouldLaunchJobWithProperParameters() throws Exception {
+    final var parametersCaptor = ArgumentCaptor.forClass(JobParameters.class);
+    when(jobLauncher.run(any(), any())).thenReturn(MetaDataInstanceFactory.createJobExecution());
+
+    exportJobService.execute();
+
+    verify(jobLauncher).run(any(), parametersCaptor.capture());
+    assertThat(parametersCaptor.getValue())
+            .satisfies(it -> assertThat(it.getString("acquirerFilename")).isEqualTo(TEST_ACQUIRER_FILE))
+            .satisfies(it -> assertThat(it.getLong("readChunkSize")).isEqualTo(TEST_CHUNK_SIZE));
+  }
+
+}
