a49257fe92dce6ccb19468bce707a9c6ac2831c8
petretiandrea <petretiandrea@gmail.com>
2023-03-28 17:45:22 +0200

    bump to spring 3.0.5 to fix aot build step

--- src/main/java/it/pagopa/gov/rtdmsexporter/domain/acquirer/ChunkWriter.java
+++ src/main/java/it/pagopa/gov/rtdmsexporter/domain/acquirer/ChunkWriter.java
@@ -3,7 +3,7 @@ package it.pagopa.gov.rtdmsexporter.domain.acquirer;
 import io.vavr.control.Try;
 import java.io.IOException;
 
-public interface ChunkWriter<T> {
+public interface ChunkWriter<T> extends AutoCloseable {
   void open() throws IOException;
   Try<Long> writeChunk(Iterable<? extends T> items);
   void close() throws IOException;
